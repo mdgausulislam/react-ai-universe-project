5d9821c68ff1b78273a1ae4f6231bccc42c89e5d
unknown <gausulislam5@gmail.com>
2023-10-11 11:31:15 +0600

    updatew components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='text-center font-bold text-4xl my-4'>
+        <h1>Al Universe Hub</h1>
+    </div>
+    );
+};
+
+export default Header;
